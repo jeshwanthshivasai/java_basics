cc72c04b7d589ab0064aac9fc4bbfe4aada63f33
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-05-24 16:53:32 +0530

    change

--- Main.java
+++ Main.java
@@ -141,9 +141,3 @@
                 
 //         }
 // }
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
-}
---
+++ Rectangle.java
@@ -0,0 +1,36 @@
+//CONSTRUCTOR
+//class declaration
+public class Rectangle {
+    //non-static members
+    public int length;
+    public int breadth;
+    public int height;
+    //create constructor
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+    //create another constructor
+    public Rectangle(int length, int breadth, int height) {
+        // this(length, breadth);
+        this.length = length;
+        this.breadth = breadth;
+        this.height = height;
+    }
+    //create a method
+    public void areaof2drect() {
+        int area = length*breadth;
+        System.out.println(area);
+    }
+    //create another method
+    public void areaof3drect() {
+        int area = length*breadth*height;
+        System.out.println(area);
+    }
+    public static void main(String[] args) {
+        Rectangle p1 = new Rectangle(10, 20);
+        p1.areaof2drect();
+        Rectangle p2 = new Rectangle(10, 20, 30);
+        p2.areaof3drect();
+    }
+}
